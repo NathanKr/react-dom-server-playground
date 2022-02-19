2142baf8692e90367b7e56a6d7a2e3fc62c8dc0c
Nathan Krasney <natankrasney@gmail.com>
2022-02-19 22:44:49 +0200

    ...

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './Person.css'
 
 const Person = ({ name, age }) => {
   return (
-    <p>
+    <p className='Person'>
       Name : {name} , Age : {age}
     </p>
   );
